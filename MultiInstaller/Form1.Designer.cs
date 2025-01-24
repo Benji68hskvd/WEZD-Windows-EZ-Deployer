a951e1ade8a8306141f4e00b1eae34eca796f272
Melvin <>
2025-01-24 16:52:22 +0100

    Add icon

--- MultiInstaller/Form1.Designer.cs
+++ MultiInstaller/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             InstallButton = new Button();
             CancelButton = new Button();
             label1 = new Label();
@@ -62,10 +63,9 @@
             // 
             // InstallButton
             // 
-            InstallButton.Location = new Point(585, 536);
-            InstallButton.Margin = new Padding(3, 4, 3, 4);
+            InstallButton.Location = new Point(512, 402);
             InstallButton.Name = "InstallButton";
-            InstallButton.Size = new Size(114, 36);
+            InstallButton.Size = new Size(100, 27);
             InstallButton.TabIndex = 0;
             InstallButton.Text = "Apply";
             InstallButton.UseVisualStyleBackColor = true;
@@ -73,10 +73,9 @@
             // 
             // CancelButton
             // 
-            CancelButton.Location = new Point(706, 536);
-            CancelButton.Margin = new Padding(3, 4, 3, 4);
+            CancelButton.Location = new Point(618, 402);
             CancelButton.Name = "CancelButton";
-            CancelButton.Size = new Size(114, 36);
+            CancelButton.Size = new Size(100, 27);
             CancelButton.TabIndex = 1;
             CancelButton.Text = "Cancel";
             CancelButton.UseVisualStyleBackColor = true;
@@ -85,19 +84,18 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(46, 47);
+            label1.Location = new Point(40, 35);
             label1.Name = "label1";
-            label1.Size = new Size(154, 20);
+            label1.Size = new Size(122, 15);
             label1.TabIndex = 2;
             label1.Text = "Application to Install :";
             // 
             // chrome
             // 
             chrome.AutoSize = true;
-            chrome.Location = new Point(43, 21);
-            chrome.Margin = new Padding(3, 4, 3, 4);
+            chrome.Location = new Point(38, 16);
             chrome.Name = "chrome";
-            chrome.Size = new Size(83, 24);
+            chrome.Size = new Size(69, 19);
             chrome.TabIndex = 3;
             chrome.Text = "Chrome";
             chrome.UseVisualStyleBackColor = true;
@@ -105,10 +103,9 @@
             // CCleaner
             // 
             CCleaner.AutoSize = true;
-            CCleaner.Location = new Point(43, 88);
-            CCleaner.Margin = new Padding(3, 4, 3, 4);
+            CCleaner.Location = new Point(38, 66);
             CCleaner.Name = "CCleaner";
-            CCleaner.Size = new Size(90, 24);
+            CCleaner.Size = new Size(74, 19);
             CCleaner.TabIndex = 4;
             CCleaner.Text = "CCleaner";
             CCleaner.UseVisualStyleBackColor = true;
@@ -116,10 +113,9 @@
             // NovaBench
             // 
             NovaBench.AutoSize = true;
-            NovaBench.Location = new Point(43, 121);
-            NovaBench.Margin = new Padding(3, 4, 3, 4);
+            NovaBench.Location = new Point(38, 91);
             NovaBench.Name = "NovaBench";
-            NovaBench.Size = new Size(106, 24);
+            NovaBench.Size = new Size(87, 19);
             NovaBench.TabIndex = 5;
             NovaBench.Text = "NovaBench";
             NovaBench.UseVisualStyleBackColor = true;
@@ -127,10 +123,9 @@
             // VLC
             // 
             VLC.AutoSize = true;
-            VLC.Location = new Point(43, 188);
-            VLC.Margin = new Padding(3, 4, 3, 4);
+            VLC.Location = new Point(38, 141);
             VLC.Name = "VLC";
-            VLC.Size = new Size(55, 24);
+            VLC.Size = new Size(47, 19);
             VLC.TabIndex = 6;
             VLC.Text = "VLC";
             VLC.UseVisualStyleBackColor = true;
@@ -138,10 +133,9 @@
             // LibreOffice
             // 
             LibreOffice.AutoSize = true;
-            LibreOffice.Location = new Point(43, 155);
-            LibreOffice.Margin = new Padding(3, 4, 3, 4);
+            LibreOffice.Location = new Point(38, 116);
             LibreOffice.Name = "LibreOffice";
-            LibreOffice.Size = new Size(104, 24);
+            LibreOffice.Size = new Size(84, 19);
             LibreOffice.TabIndex = 7;
             LibreOffice.Text = "LibreOffice";
             LibreOffice.UseVisualStyleBackColor = true;
@@ -151,10 +145,9 @@
             AutoDeleteInstaller.AutoSize = true;
             AutoDeleteInstaller.Checked = true;
             AutoDeleteInstaller.CheckState = CheckState.Checked;
-            AutoDeleteInstaller.Location = new Point(14, 547);
-            AutoDeleteInstaller.Margin = new Padding(3, 4, 3, 4);
+            AutoDeleteInstaller.Location = new Point(12, 410);
             AutoDeleteInstaller.Name = "AutoDeleteInstaller";
-            AutoDeleteInstaller.Size = new Size(167, 24);
+            AutoDeleteInstaller.Size = new Size(132, 19);
             AutoDeleteInstaller.TabIndex = 10;
             AutoDeleteInstaller.Text = "Auto Delete Installer";
             AutoDeleteInstaller.UseVisualStyleBackColor = true;
@@ -162,9 +155,9 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(295, 47);
+            label2.Location = new Point(258, 35);
             label2.Name = "label2";
-            label2.Size = new Size(148, 20);
+            label2.Size = new Size(119, 15);
             label2.TabIndex = 12;
             label2.Text = "Windows Activation :";
             // 
@@ -172,10 +165,9 @@
             // 
             radioButton1.AutoSize = true;
             radioButton1.Checked = true;
-            radioButton1.Location = new Point(34, 21);
-            radioButton1.Margin = new Padding(3, 4, 3, 4);
+            radioButton1.Location = new Point(30, 16);
             radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(84, 24);
+            radioButton1.Size = new Size(69, 19);
             radioButton1.TabIndex = 13;
             radioButton1.TabStop = true;
             radioButton1.Text = "Nothing";
@@ -184,10 +176,9 @@
             // HWID
             // 
             HWID.AutoSize = true;
-            HWID.Location = new Point(34, 55);
-            HWID.Margin = new Padding(3, 4, 3, 4);
+            HWID.Location = new Point(30, 41);
             HWID.Name = "HWID";
-            HWID.Size = new Size(70, 24);
+            HWID.Size = new Size(56, 19);
             HWID.TabIndex = 14;
             HWID.Text = "HWID";
             HWID.UseVisualStyleBackColor = true;
@@ -195,10 +186,9 @@
             // KMS38
             // 
             KMS38.AutoSize = true;
-            KMS38.Location = new Point(34, 88);
-            KMS38.Margin = new Padding(3, 4, 3, 4);
+            KMS38.Location = new Point(30, 66);
             KMS38.Name = "KMS38";
-            KMS38.Size = new Size(76, 24);
+            KMS38.Size = new Size(61, 19);
             KMS38.TabIndex = 15;
             KMS38.Text = "KMS38";
             KMS38.UseVisualStyleBackColor = true;
@@ -206,10 +196,9 @@
             // WinOnlineKMS
             // 
             WinOnlineKMS.AutoSize = true;
-            WinOnlineKMS.Location = new Point(34, 121);
-            WinOnlineKMS.Margin = new Padding(3, 4, 3, 4);
+            WinOnlineKMS.Location = new Point(30, 91);
             WinOnlineKMS.Name = "WinOnlineKMS";
-            WinOnlineKMS.Size = new Size(107, 24);
+            WinOnlineKMS.Size = new Size(87, 19);
             WinOnlineKMS.TabIndex = 16;
             WinOnlineKMS.Text = "Online KMS";
             WinOnlineKMS.UseVisualStyleBackColor = true;
@@ -221,10 +210,9 @@
             panel1.Controls.Add(HWID);
             panel1.Controls.Add(KMS38);
             panel1.Controls.Add(WinOnlineKMS);
-            panel1.Location = new Point(290, 59);
-            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Location = new Point(254, 44);
             panel1.Name = "panel1";
-            panel1.Size = new Size(210, 182);
+            panel1.Size = new Size(184, 137);
             panel1.TabIndex = 18;
             // 
             // panel2
@@ -237,19 +225,17 @@
             panel2.Controls.Add(VLC);
             panel2.Controls.Add(NovaBench);
             panel2.Controls.Add(LibreOffice);
-            panel2.Location = new Point(41, 59);
-            panel2.Margin = new Padding(3, 4, 3, 4);
+            panel2.Location = new Point(36, 44);
             panel2.Name = "panel2";
-            panel2.Size = new Size(228, 271);
+            panel2.Size = new Size(200, 204);
             panel2.TabIndex = 19;
             // 
             // TeamViewer
             // 
             TeamViewer.AutoSize = true;
-            TeamViewer.Location = new Point(43, 221);
-            TeamViewer.Margin = new Padding(3, 4, 3, 4);
+            TeamViewer.Location = new Point(38, 166);
             TeamViewer.Name = "TeamViewer";
-            TeamViewer.Size = new Size(112, 24);
+            TeamViewer.Size = new Size(90, 19);
             TeamViewer.TabIndex = 8;
             TeamViewer.Text = "TeamViewer";
             TeamViewer.UseVisualStyleBackColor = true;
@@ -257,10 +243,9 @@
             // Firefox
             // 
             Firefox.AutoSize = true;
-            Firefox.Location = new Point(43, 55);
-            Firefox.Margin = new Padding(3, 4, 3, 4);
+            Firefox.Location = new Point(38, 41);
             Firefox.Name = "Firefox";
-            Firefox.Size = new Size(76, 24);
+            Firefox.Size = new Size(61, 19);
             Firefox.TabIndex = 9;
             Firefox.Text = "Firefox";
             Firefox.UseVisualStyleBackColor = true;
@@ -268,9 +253,9 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(295, 267);
+            label3.Location = new Point(258, 200);
             label3.Name = "label3";
-            label3.Size = new Size(127, 20);
+            label3.Size = new Size(102, 15);
             label3.TabIndex = 20;
             label3.Text = "Office Activation :";
             // 
@@ -280,20 +265,18 @@
             panel3.Controls.Add(radioButton8);
             panel3.Controls.Add(OfficeOnlineKMS);
             panel3.Controls.Add(Ohook);
-            panel3.Location = new Point(290, 280);
-            panel3.Margin = new Padding(3, 4, 3, 4);
+            panel3.Location = new Point(254, 210);
             panel3.Name = "panel3";
-            panel3.Size = new Size(210, 150);
+            panel3.Size = new Size(184, 113);
             panel3.TabIndex = 21;
             // 
             // radioButton8
             // 
             radioButton8.AutoSize = true;
             radioButton8.Checked = true;
-            radioButton8.Location = new Point(34, 24);
-            radioButton8.Margin = new Padding(3, 4, 3, 4);
+            radioButton8.Location = new Point(30, 18);
             radioButton8.Name = "radioButton8";
-            radioButton8.Size = new Size(84, 24);
+            radioButton8.Size = new Size(69, 19);
             radioButton8.TabIndex = 25;
             radioButton8.TabStop = true;
             radioButton8.Text = "Nothing";
@@ -302,10 +285,9 @@
             // OfficeOnlineKMS
             // 
             OfficeOnlineKMS.AutoSize = true;
-            OfficeOnlineKMS.Location = new Point(34, 91);
-            OfficeOnlineKMS.Margin = new Padding(3, 4, 3, 4);
+            OfficeOnlineKMS.Location = new Point(30, 68);
             OfficeOnlineKMS.Name = "OfficeOnlineKMS";
-            OfficeOnlineKMS.Size = new Size(107, 24);
+            OfficeOnlineKMS.Size = new Size(87, 19);
             OfficeOnlineKMS.TabIndex = 23;
             OfficeOnlineKMS.Text = "Online KMS";
             OfficeOnlineKMS.UseVisualStyleBackColor = true;
@@ -313,10 +295,9 @@
             // Ohook
             // 
             Ohook.AutoSize = true;
-            Ohook.Location = new Point(34, 57);
-            Ohook.Margin = new Padding(3, 4, 3, 4);
+            Ohook.Location = new Point(30, 43);
             Ohook.Name = "Ohook";
-            Ohook.Size = new Size(74, 24);
+            Ohook.Size = new Size(61, 19);
             Ohook.TabIndex = 22;
             Ohook.Text = "Ohook";
             Ohook.UseVisualStyleBackColor = true;
@@ -326,10 +307,9 @@
             UseCurDir.AutoSize = true;
             UseCurDir.Checked = true;
             UseCurDir.CheckState = CheckState.Checked;
-            UseCurDir.Location = new Point(14, 513);
-            UseCurDir.Margin = new Padding(3, 4, 3, 4);
+            UseCurDir.Location = new Point(12, 385);
             UseCurDir.Name = "UseCurDir";
-            UseCurDir.Size = new Size(172, 24);
+            UseCurDir.Size = new Size(139, 19);
             UseCurDir.TabIndex = 23;
             UseCurDir.Text = "Use Current Directory";
             UseCurDir.UseVisualStyleBackColor = true;
@@ -337,36 +317,35 @@
             // panel4
             // 
             panel4.BorderStyle = BorderStyle.FixedSingle;
-            panel4.Location = new Point(520, 59);
-            panel4.Margin = new Padding(3, 4, 3, 4);
+            panel4.Location = new Point(455, 44);
             panel4.Name = "panel4";
-            panel4.Size = new Size(482, 439);
+            panel4.Size = new Size(422, 330);
             panel4.TabIndex = 24;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(525, 47);
+            label4.Location = new Point(459, 35);
             label4.Name = "label4";
-            label4.Size = new Size(129, 20);
+            label4.Size = new Size(103, 15);
             label4.TabIndex = 0;
             label4.Text = "Office Instalation :";
             // 
             // labelStatus
             // 
             labelStatus.AutoSize = true;
-            labelStatus.Location = new Point(92, 410);
+            labelStatus.Location = new Point(80, 308);
             labelStatus.Name = "labelStatus";
-            labelStatus.Size = new Size(48, 20);
+            labelStatus.Size = new Size(38, 15);
             labelStatus.TabIndex = 25;
             labelStatus.Text = "V1.2.0";
             labelStatus.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1061, 595);
+            ClientSize = new Size(928, 446);
             Controls.Add(labelStatus);
             Controls.Add(label4);
             Controls.Add(panel4);
@@ -380,7 +359,7 @@
             Controls.Add(AutoDeleteInstaller);
             Controls.Add(CancelButton);
             Controls.Add(InstallButton);
-            Margin = new Padding(3, 4, 3, 4);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             Text = "MultiInstaller";
             panel1.ResumeLayout(false);
---
+++ MultiInstaller/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyCompany("MultiInstaller")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyDescription("MultiInstaller : Installer multiples apps")]
+[assembly: AssemblyCopyright("Copyright © 2024-2025 MultiInstaller. - All rights reserved.")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
+[assembly: AssemblyProduct("MultiInstaller")]
+[assembly: AssemblyTitle("MultiInstaller")]
+[assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/Benji68hskvd/MultiInstaller")]
+[assembly: AssemblyVersion("1.2.0.0")]
